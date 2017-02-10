fced354ac16e8d009a63cc4e478aa6272354b707
Chris LE VAN PHUNG <chle-van@e2r7p1.42.fr>
2017-02-10 07:08:49 +0100

    pas grd chos

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: chle-van <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/19 09:42:40 by chle-van          #+#    #+#             */
-/*   Updated: 2017/02/07 07:50:14 by chle-van         ###   ########.fr       */
+/*   Updated: 2017/02/10 07:04:35 by chle-van         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 16
+# define BUFF_SIZE 100
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/uio.h>
